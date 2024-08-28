e293d31302c3853d56961b46b34acd9e2296bc12
Joshua Harbaugh <jharb@unc.edu>
2024-08-28 09:22:23 -0400

    cq00-1

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,12 @@
+"""Challenge Question 00"""
+
+__author__ = "730796549"
+
+
+def mimic(message: str) -> str:
+    """Print your message back to you"""
+    return message
+
+
+def main():
+    print(mimic(message="Howdy!"))
