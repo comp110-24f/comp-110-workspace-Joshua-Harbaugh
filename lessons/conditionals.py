8f9ac2259b860aec1a7e8bd75ac8511a1765c221
Joshua Harbaugh <jharb@unc.edu>
2024-09-09 09:54:15 -0400

    conditionals 3

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -10,4 +10,20 @@ def less_than_10(num: int) -> None:
     print("end of function")
 
 
-less_than_10(num=554)
+def wakeup(alarm: bool) -> str:
+    """return a message based on whether or not the alarm is going off"""
+    if alarm is True:
+        return "Wake up"
+    else:
+        return "keep sleeping buster"
+
+
+def check_first_letter(word: str, letter: str) -> str:
+    """Checks if a letter is the first character of a word"""
+    if letter == word[0]:
+        return "Match"
+    else:
+        return "No Match"
+
+
+print(check_first_letter(word="beef", letter="d"))
